f05eaf4bb781aff079c24081c674ab202ef2adf5
Ashraf Ahmed <116742552+Ashrafahmed0222@users.noreply.github.com>
2023-04-14 15:27:34 +0200

    Add files via upload

--- HAL/CLCD_Driver/CLCD_program.c
+++ HAL/CLCD_Driver/CLCD_program.c
@@ -8,65 +8,54 @@
 #include "CLCD_config.h"
 #include "CLCD_private.h"
 #include "CLCD_externalChars.h"
-
 void CLCD_voidInit(void) {
-#if CLCD_MODE == 8
-	_delay_ms(50);
 
-	DIO_enumSetPortDirection(CLCD_PORT, OUTPUT_PORT);
-	DIO_enumSetPinDirection(CLCD_CONTROL_PORT, CLCD_RS, DIO_PIN_OUTPUT);
-	DIO_enumSetPinDirection(CLCD_CONTROL_PORT, CLCD_RW, DIO_PIN_OUTPUT);
-	DIO_enumSetPinDirection(CLCD_CONTROL_PORT, CLCD_EN, DIO_PIN_OUTPUT);
+	DIO_enumSetPortDirection( CLCD_DATA_PORT, 0xFF);
+	DIO_enumSetPinDirection( CLCD_CONTROL_PORT, CLCD_RS, DIO_PIN_OUTPUT);
+	DIO_enumSetPinDirection( CLCD_CONTROL_PORT, CLCD_RW, DIO_PIN_OUTPUT);
+	DIO_enumSetPinDirection( CLCD_CONTROL_PORT, CLCD_EN, DIO_PIN_OUTPUT);
+
+	_delay_ms(100);
 
 	CLCD_voidSendCommand(lcd_Home);
-	_delay_ms(1);
-	CLCD_voidSendCommand(EIGHT_BITS);
-	_delay_ms(1);
-	CLCD_voidSendCommand(lcd_DispalyOn_CursorOff);
-	_delay_ms(1);
+	_delay_ms(5);
+
+	CLCD_voidSendCommand( lcd_DispalyOn_CursorOff);
+	_delay_ms(5);
+
 	CLCD_voidClearScreen();
 
-	CLCD_voidSendCommand(lcd_EntryMode);
-	_delay_ms(1);
+	_delay_ms(5);
 
-#elif CLCD_MODE == 4
-#endif
+	CLCD_voidSendCommand( lcd_EntryMode);
+	_delay_ms(10);
 
 }
 
 void CLCD_voidSendData(u8 copy_u8Data) {
-#if CLCD_MODE == 8
-	DIO_enumSetPortValue(CLCD_PORT, copy_u8Data);
+
 	DIO_enumSetPinValue(CLCD_CONTROL_PORT, CLCD_RS, DIO_PIN_HIGH);
 	DIO_enumSetPinValue(CLCD_CONTROL_PORT, CLCD_RW, DIO_PIN_LOW);
+	DIO_enumSetPortValue(CLCD_DATA_PORT, copy_u8Data);
 	CLCD_voidSendFallingEdge();
-
-#elif CLCD_MODE == 4
-
-#endif
-	_delay_ms(1);
 
 }
 
 void CLCD_voidSendCommand(u8 Copy_u8Command) {
-#if CLCD_MODE == 8
-	DIO_enumSetPortValue(CLCD_PORT, Copy_u8Command);
+
 	DIO_enumSetPinValue(CLCD_CONTROL_PORT, CLCD_RS, DIO_PIN_LOW);
 	DIO_enumSetPinValue(CLCD_CONTROL_PORT, CLCD_RW, DIO_PIN_LOW);
+	DIO_enumSetPortValue(CLCD_DATA_PORT, Copy_u8Command);
 	CLCD_voidSendFallingEdge();
-
-#elif CLCD_MODE == 4
-
-#endif
-	_delay_ms(1);
 
 }
 
 void CLCD_voidSendFallingEdge(void) {
 	DIO_enumSetPinValue(CLCD_CONTROL_PORT, CLCD_EN, DIO_PIN_HIGH);
 	_delay_ms(1);
-	DIO_enumSetPinValue(CLCD_CONTROL_PORT, CLCD_RS, DIO_PIN_LOW);
+	DIO_enumSetPinValue(CLCD_CONTROL_PORT, CLCD_EN, DIO_PIN_LOW);
 	_delay_ms(1);
+
 }
 
 void CLCD_voidClearScreen(void) {
@@ -77,7 +66,7 @@ void CLCD_voidClearScreen(void) {
 
 void CLCD_voidSendString(const u8 *copy_u8ptrString) {
 	u8 Loc_Iterator = 0;
-	while (copy_u8ptrString[Loc_Iterator] != "\0") {
+	while (copy_u8ptrString[Loc_Iterator] != '\0') {
 		CLCD_voidSendData(copy_u8ptrString[Loc_Iterator]);
 		Loc_Iterator++;
 	}
@@ -96,26 +85,26 @@ void CLCD_voidSetPosition(u8 Copy_u8ROW, u8 Copy_u8Column) {/*DDRAM*/
 	_delay_ms(1);
 }
 
-void CLCD_voidSendExternAlChar(u8 Copy_u8ROW, u8 Copy_u8COLUMN){
-	u8 Loc_Iterator =0;
+void CLCD_voidSendExternAlChar(u8 Copy_u8ROW, u8 Copy_u8COLUMN) {
+	u8 Loc_Iterator = 0;
 
 	CLCD_voidSendCommand(lcd_CGRAM);
 
-	for(Loc_Iterator =0 ; Loc_Iterator < (sizeof(CLCD_u8ExternalChars)/sizeof(CLCD_u8ExternalChars[0]));Loc_Iterator++){
+	for (Loc_Iterator = 0;
+			Loc_Iterator
+					< (sizeof(CLCD_u8ExternalChars)
+							/ sizeof(CLCD_u8ExternalChars[0]));
+			Loc_Iterator++) {
 		CLCD_voidSendData(CLCD_u8ExternalChars[Loc_Iterator]);
 	}
 
-	CLCD_voidSetPosition(Copy_u8ROW,Copy_u8COLUMN);
+	CLCD_voidSetPosition(Copy_u8ROW, Copy_u8COLUMN);
 
-	for(Loc_Iterator =0 ; Loc_Iterator< 8 ; Loc_Iterator++){
-			CLCD_voidSendData(Loc_Iterator);
-		}
+	for (Loc_Iterator = 0; Loc_Iterator < 8; Loc_Iterator++) {
+		CLCD_voidSendData(Loc_Iterator);
+	}
 
 }
-
-
-
-
 
 
 
